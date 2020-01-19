bf57125626af5c2f0549e1df032672ecb772b7c1
AmitP <amit.b.pal@gmail.com>
2020-01-18 17:58:48 -0800

    Update code partition

--- java/leetcode/src/main/java/leetcode/PartitionLabels.java
+++ java/leetcode/src/main/java/leetcode/PartitionLabels.java
@@ -1,52 +1,79 @@
 package leetcode;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class PartitionLabels {
 	public List<Integer> partitionLabels(String s) {
 
 		List<Integer> resultList = new ArrayList<>();
 
-		Set<Character> uniqueSet = new TreeSet<Character>();
+		Set<Character> uniqueSet = new LinkedHashSet<Character>();
 
 		List<Character> charList = new ArrayList<Character>();
 
 		for (Character c : s.toCharArray()) {
 			charList.add(c);
+			uniqueSet.add(c);
+
 		}
 		
-		uniqueSet.addAll(charList);
+		System.out.println(s);
 
-		
+		int lastLength = -1;
 		while (!uniqueSet.isEmpty()) {
-			resultList.add(this.getPartition(charList, uniqueSet, 0));
+			
+			int length = this.getPartition(charList, uniqueSet);
+			resultList.add(length-lastLength);
+
+			System.out.println(s.substring(lastLength+1,length+1));
+
+			
+			lastLength = length;
+			
+			
 		}
 
 		System.out.println(resultList);
+
+		
 		return resultList;
 	}
 
-	private int getPartition(List<Character> charList, Set<Character> uniqueSet, int charArrIndex) {
-		int partitionLenght = 0;
+	private int getPartition(List<Character> charList, Set<Character> uniqueSet) {
+		int partitionLenght = -1;
 		List<Character> processedCharList = new ArrayList<Character>();
-		int partitionStart = 0;
+		int partitionFirstIndex =-1;
 		for(Character c : uniqueSet) {
-			int tLength = charList.lastIndexOf(c);
-			if (partitionLenght == 0) {
-				partitionLenght = tLength;
-				partitionStart = charList.indexOf(c);
-			}else if (tLength> partitionLenght && charList.indexOf(c) > partitionStart) {
-				partitionLenght = tLength;
-			}else {
-				uniqueSet.removeAll(processedCharList);
-				break;
+			int lastIndexCA = charList.lastIndexOf(c);
+			
+			int firstIndexCA = charList.indexOf(c);
+			
+			if (partitionLenght ==-1) {
+				partitionLenght = lastIndexCA;
+				partitionFirstIndex = firstIndexCA;
 			}
+
+			
+			if (partitionLenght < lastIndexCA && firstIndexCA > partitionLenght ) {
+				uniqueSet.removeAll(processedCharList);
+				return partitionLenght;
+			}
+			
+			if (partitionLenght < lastIndexCA ) {
+				partitionLenght = lastIndexCA;
+			}
+			if(firstIndexCA > partitionFirstIndex ) {
+
+				partitionFirstIndex = firstIndexCA;
+			}
+			
 			processedCharList.add(c);
 		}
+		uniqueSet.removeAll(processedCharList);
+
 		return partitionLenght;
 	}
 }
--- java/leetcode/src/test/java/leetcode/PartitionLabelsTest.java
+++ java/leetcode/src/test/java/leetcode/PartitionLabelsTest.java
@@ -9,12 +9,20 @@ import org.junit.Test;
 public class PartitionLabelsTest {
 	private PartitionLabels testObject = new PartitionLabels();
 
-	@Test
+	//@Test
 	public void leetCodeSampleTest() {
 		
 		List<Integer> result = testObject.partitionLabels("ababcbacadefegdehijhklij");
 		//assertEquals(4, result);
 	}
 
+
+	
+	@Test
+	public void leetCodeSampleTest2() {
+		
+		List<Integer> result = testObject.partitionLabels("caedbdedda");
+		//assertEquals(4, result);
+	}
 
 }
