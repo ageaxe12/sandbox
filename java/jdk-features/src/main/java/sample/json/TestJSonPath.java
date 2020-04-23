a4eece3417650c670d0c8f7f446c97a8423b0a08
apal <apal2@cisco.com>
2020-04-23 11:57:46 -0700

    Added code for json parsing

--- java/jdk-features/src/main/java/sample/json/TestJSonPath.java
+++ java/jdk-features/src/main/java/sample/json/TestJSonPath.java
@@ -1,13 +1,20 @@
 package sample.json;
 
 import java.io.InputStream;
+import java.text.MessageFormat;
 import java.util.List;
 
 import io.restassured.path.json.JsonPath;
 
 public class TestJSonPath {
 
+
 	public static void main(String[] args) {
+		
+		System.out.println(MessageFormat.format("https://{0}/portal.html", "xxx","yyyy"));
+	}
+	
+	public static void main1(String[] args) {
 		InputStream fileIO = TestJSonPath.class.getClassLoader().getResourceAsStream("rules-response.json");
 
 		// jsonPath.param("className","com.pingidentity.pa.policy.GroovyPolicyInterceptor").get("items.findAll
